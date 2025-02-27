64005040d3c0420d20b73de729f7aaa7fd606568
juanfelipe <jimenezlozadajuanfelipe@gmail.com>
2025-02-26 20:18:54 -0500

    Primer commit

---
+++ aws_inventory_management/inventory/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from .views import Index, MyLogoutView, SignUpView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', Index.as_view(), name='index'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='inventory/login.html'), name='login'),
+    path('logout/', MyLogoutView.as_view(template_name='inventory/logout.html'), name='logout'),
+
+    
+]
